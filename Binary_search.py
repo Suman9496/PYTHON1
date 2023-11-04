59314d2ba00266e51a49c1d4224ec8e0f2b3af29
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-11-04 23:18:28 +0530

    binary search

---
+++ Binary_search.py
@@ -0,0 +1,24 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid  # Element found, return its index
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1  # Element not found
+
+
+# Usage example
+if __name__ == "__main__":
+    sorted_list = [1, 3, 5, 7, 9, 11, 13, 15]
+    target = 7
+    result = binary_search(sorted_list, target)
+    if result != -1:
+        print(f"Element {target} found at index {result}.")
+    else:
+        print(f"Element {target} not found in the list.")
