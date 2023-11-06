320ba9005aebd7cdbdafd3718d980fb0de7e9347
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-11-06 22:49:15 +0530

    Array

---
+++ array.py
@@ -0,0 +1,12 @@
+# Creating an array
+my_array = [1, 2, 3, 4, 5]
+
+# Accessing elements
+print(my_array[0])  # Accessing the first element
+
+# Modifying elements
+my_array[2] = 10
+
+# Looping through elements
+for element in my_array:
+    print(element)
