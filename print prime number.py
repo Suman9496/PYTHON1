c7eb9c72e28d59d0ffa3c469db50fb2d5a0fc426
Shuman kumari <95907851+Suman9496@users.noreply.github.com>
2023-11-10 21:58:18 +0530

    This code will prompt the user for a number and then print a list of all prime numbers up to that number.

---
+++ print prime number.py
@@ -0,0 +1,17 @@
+def prime_numbers(n):
+    primes = []
+    for i in range(2, n + 1):
+        is_prime = True
+        for j in range(2, int(i**0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+
+    return primes
+
+
+n = int(input("Enter a number: "))
+
+print(prime_numbers(n))
